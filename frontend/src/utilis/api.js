a9ed886cc189da87a741b6fd74e8b803476237fc
Ganapaved <vedvedanmoolya@gmail.com>
2025-10-02 21:47:38 +0530

    deplyoemnt-VI

--- frontend/src/utilis/api.js
+++ frontend/src/utilis/api.js
@@ -1,6 +1,4 @@
-const API_BASE_URL = import.meta.env.PROD 
-  ? 'https://rigveda-backend.onrender.com'  // Your Render URL
-  : 'http://localhost:5000';
+const API_BASE_URL = '';
 export const apiCall = async (endpoint , options ={})=>{
     const url = `${API_BASE_URL}${endpoint}`;
 
