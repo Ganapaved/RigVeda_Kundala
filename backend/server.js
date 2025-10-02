01a052153e3455df22a07e9ec0845d515da31b16
Ganapaved <vedvedanmoolya@gmail.com>
2025-10-02 21:51:47 +0530

    deplyoemnt-VII

--- backend/server.js
+++ backend/server.js
@@ -12,9 +12,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: process.env.NODE_ENV === 'production' 
-  ? "https://rig-veda-kundala-8job.vercel.app/"
-  : "http://localhost:3000",
+  origin: "https://rig-veda-kundala-8job.vercel.app/",
   credentials: true
 }));
 app.use(express.json());
